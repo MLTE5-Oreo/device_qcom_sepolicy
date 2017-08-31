2f8522d7aff5bb5f19ddfa38a83fca079e935ca6
cb <billchen1977@gmail.com>
2017-08-31 16:34:04 +0800

    Fix build for AOSP
    Revert "NETMGR: Use HIDL to talk to netd"
    
    This reverts commit 4828dd071564322dbc392cdda24dd53ba071842a.
    
    Revert "wfd : Changes for Secure WFD And"
    
    This reverts commit 100cf932e1cde410121dd1e0c4ff00cf294fb622.
    
    Revert "wfd : Added required sepolicy rules"
    
    This reverts commit fb797d966e9e58a32d0921c6a32f4a0908bfbb29.
    
    Conflicts:
    	common/file_contexts
    	common/hwservice.te
    	common/hwservice_contexts
    	common/wfdservice.te
    	private/wfdservice.te
    	public/wfdservice.te
    
    Revert "Add domain for ssg tz daemon"
    
    This reverts commit 9da1b4f32f4c30c9c590071f7bdf098a5756e07b.
    
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    
    Revert "Add config for SSG QMI Gateway Daemon"
    
    This reverts commit 61000cecd247b6a00ed18ca2299a093f691e4c42.
    
    Conflicts:
    	common/file_contexts
    
    Revert "Add domain for ssg apps"
    
    This reverts commit b5f0b0057b4744d3d2a14dec146e4ca7bc995d85.
    
    Conflicts:
    	Android.mk
    	ssg/ssg_app.te
    
    Revert "sepolicy: testValidSeappContexts is failing in CtsSecurityHostTestCases"
    
    This reverts commit 3336b359d177c6ce6d460a2699cb53150baf00d7.

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,7 +1,6 @@
 # Board specific SELinux policy variable definitions
 BOARD_SEPOLICY_DIRS += \
     device/qcom/sepolicy/common \
-    device/qcom/sepolicy/ssg \
     device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
